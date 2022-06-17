4787a6813c7142fd0a13b12ab9a10778ec3fce45
Antonio Huerta Reyes <anhr9728@gmail.com>
2022-06-16 20:24:52 -0500

    Improve code and fix navBar styles and toggle

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,20 +1,54 @@
-import React from 'react'
-type NavBarProps ={
-    labels:Array<string>
+
+import * as Icon from 'react-bootstrap-icons';
+
+
+type NavBarProps = {
+  labels: Array<string>
 }
-const NavBar = ({labels}:NavBarProps) =>  {
+
+const NavBar = ({labels}: NavBarProps) => {
   return (
-    <nav className='m-3 '>
-        <ul className='nav nav-pills  bg-light'>
-       { labels.map((label) => (
-             <li className="nav-item mx-4">
-             <a className="nav-link h6 navbarTextColor " aria-current="page" href="#">
-                {label}
-             </a>
-           </li>
-        ))}
-        </ul>
-    </nav>
+    <>
+      <nav className="navbar navbar-expand-lg bg-light custom-navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">LOGO MIGALA</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                  aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {labels.map((label, index) => (
+                <li key={`${index}${label}`} className="nav-item nav-item-custom">
+                  <a className="nav-link" aria-current="page" href="#">
+                    {label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+        </div>
+      </nav>
+
+      <style jsx>{`
+
+        .custom-navbar {
+          background-color: transparent !important;
+          padding: 15px 20px;
+        }
+
+        .nav-item-custom > a{
+          color: black !important;
+          font-size: 18px;
+          font-weight: 500;
+          padding: 0 20px;
+        }
+
+      `}</style>
+    </>
   )
 }
 
--- pages/Start.tsx
+++ pages/Start.tsx
@@ -1,17 +1,9 @@
-import NavBar from "../components/NavBar";
 import * as Icon from 'react-bootstrap-icons';
 //estoy importando los iconos de bootstrap, al mismo tiempo, sabiendo que  las barras son reutilizables, manejo el componente NavBar con propiedades, le estoy pasando un array con las etiquetas... simple.
-let titleLabel: string[] = [
-  "Inicio",
-  "Nosotrxs",
-  "Actividades",
-  "Proyectos",
-  "Archivo",
-];
+
 const Start = () => {
   return (
     <>
-      <NavBar labels={titleLabel} />
       <section className="m-sm-3 m-xl-5 row">
         <div className="col-sm-12 col-lg-5 mx-auto">
           <h1 className="display-5 fw-bolder">
@@ -52,11 +44,11 @@ const Start = () => {
           <div className="d-flex justify-content-between align-items-center">
             <h6>Nuestras Redes Sociales</h6>
             <div className=" w-50 d-flex justify-content-between">
-            <Icon.Facebook  size={56}/>
-            <Icon.Instagram size={56}/>
-            <Icon.Twitter size={56}/>
-            <Icon.Youtube size={56}/>
-            <Icon.Tiktok size={56}/>
+              <Icon.Facebook  size={56}/>
+              <Icon.Instagram size={56}/>
+              <Icon.Twitter size={56}/>
+              <Icon.Youtube size={56}/>
+              <Icon.Tiktok size={56}/>
             </div>
           </div>
         </div>
